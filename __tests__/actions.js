4af9a21267ce2d400019d1365425a3c70dadb7ad
Mike Piccolo <mfpiccolo@gmail.com>
2018-06-22 17:48:13 -0700

    Add test for resources store with MobX

--- __tests__/actions.js
+++ __tests__/actions.js
@@ -18,10 +18,112 @@ import normalizedGraphQlChecklistPayload
 import resourcesReducer from "../src/resourcesReducer";
 
 describe("actions", () => {
-  describe("updateResources", () => {
+  describe("Redux", () => {
+    describe("updateResources", () => {
+      describe("JsonApi", () => {
+        test("dispatches MERGE_RESOURCES for each ", () => {
+          const dispatch = jest.fn();
+
+          const tasksMergeResourcesAction = {
+            resourceType: "tasks",
+            resourcesById: normalizedJsonApiTasksPayload,
+            type: "MERGE_RESOURCES"
+          };
+
+          const checklistsMergeResourcesAction = {
+            resourceType: "checklists",
+            resourcesById: normalizedJsonApiChecklistsPayload,
+            type: "MERGE_RESOURCES"
+          };
+
+          updateResources(jsonApiPayload, dispatch);
+          expect(dispatch).toBeCalledWith(tasksMergeResourcesAction);
+          expect(dispatch).toBeCalledWith(checklistsMergeResourcesAction);
+          expect(dispatch).toMatchSnapshot();
+        });
+      });
+
+      describe("GraphQl", () => {
+        test("dispatches MERGE_RESOURCES for each ", () => {
+          const dispatch = jest.fn();
+
+          const tasksMergeResourcesAction = {
+            resourceType: "tasks",
+            resourcesById: normalizedGraphQlTaskPayload,
+            type: "MERGE_RESOURCES"
+          };
+
+          const checklistsMergeResourcesAction = {
+            resourceType: "checklists",
+            resourcesById: normalizedGraphQlChecklistPayload,
+            type: "MERGE_RESOURCES"
+          };
+
+          updateResources(graphQlPayload, dispatch);
+          expect(dispatch).toBeCalledWith(tasksMergeResourcesAction);
+          expect(dispatch).toBeCalledWith(checklistsMergeResourcesAction);
+          expect(dispatch).toMatchSnapshot();
+        });
+      });
+    });
+
+    describe("updateResource", () => {
+      test("dispatches update action for a single resource", () => {
+        const dispatch = jest.fn();
+
+        const checklist = {
+          id: 1,
+          type: "checklists",
+          attributes: { name: "Onboarding Rest" },
+          links: { self: "http://example.com/checklists/1" },
+          relationships: {
+            tasks: {
+              data: [{ id: 1, type: "tasks" }, { id: 2, type: "tasks" }]
+            }
+          }
+        };
+
+        const updateAction = {
+          type: "ADD_OR_REPLACE_RESOURCE_BY_ID",
+          resourceType: checklist.type,
+          id: checklist.id,
+          attributes: checklist.attributes,
+          links: checklist.links,
+          relationships: checklist.relationships
+        };
+
+        updateResource(checklist, dispatch);
+        expect(dispatch).toBeCalledWith(updateAction);
+        expect(dispatch).toMatchSnapshot();
+      });
+    });
+
+    describe("removeResource", () => {
+      test("dispatches remove resource action", () => {
+        const dispatch = jest.fn();
+
+        const checklist = {
+          id: 1,
+          type: "checklists"
+        };
+
+        const deleteAction = {
+          type: "REMOVE_RESOURCE_BY_ID",
+          resourceType: checklist.type,
+          id: checklist.id
+        };
+
+        removeResource(checklist, dispatch);
+        expect(dispatch).toBeCalledWith(deleteAction);
+        expect(dispatch).toMatchSnapshot();
+      });
+    });
+  });
+
+  describe("Mobx", () => {
     describe("JsonApi", () => {
       test("dispatches MERGE_RESOURCES for each ", () => {
-        const dispatch = jest.fn();
+        const store = {};
 
         const tasksMergeResourcesAction = {
           resourceType: "tasks",
@@ -35,84 +137,9 @@ describe("actions", () => {
           type: "MERGE_RESOURCES"
         };
 
-        updateResources(jsonApiPayload, dispatch);
-        expect(dispatch).toBeCalledWith(tasksMergeResourcesAction);
-        expect(dispatch).toBeCalledWith(checklistsMergeResourcesAction);
-        expect(dispatch).toMatchSnapshot();
+        updateResources(jsonApiPayload, store);
+        expect(store).toMatchSnapshot();
       });
-    });
-
-    describe("GraphQl", () => {
-      test("dispatches MERGE_RESOURCES for each ", () => {
-        const dispatch = jest.fn();
-
-        const tasksMergeResourcesAction = {
-          resourceType: "tasks",
-          resourcesById: normalizedGraphQlTaskPayload,
-          type: "MERGE_RESOURCES"
-        };
-
-        const checklistsMergeResourcesAction = {
-          resourceType: "checklists",
-          resourcesById: normalizedGraphQlChecklistPayload,
-          type: "MERGE_RESOURCES"
-        };
-
-        updateResources(graphQlPayload, dispatch);
-        expect(dispatch).toBeCalledWith(tasksMergeResourcesAction);
-        expect(dispatch).toBeCalledWith(checklistsMergeResourcesAction);
-        expect(dispatch).toMatchSnapshot();
-      });
-    });
-  });
-
-  describe("updateResource", () => {
-    test("dispatches update action for a single resource", () => {
-      const dispatch = jest.fn();
-
-      const checklist = {
-        id: 1,
-        type: "checklists",
-        attributes: { name: "Onboarding Rest" },
-        links: { self: "http://example.com/checklists/1" },
-        relationships: {
-          tasks: { data: [{ id: 1, type: "tasks" }, { id: 2, type: "tasks" }] }
-        }
-      };
-
-      const updateAction = {
-        type: "ADD_OR_REPLACE_RESOURCE_BY_ID",
-        resourceType: checklist.type,
-        id: checklist.id,
-        attributes: checklist.attributes,
-        links: checklist.links,
-        relationships: checklist.relationships
-      };
-
-      updateResource(checklist, dispatch);
-      expect(dispatch).toBeCalledWith(updateAction);
-      expect(dispatch).toMatchSnapshot();
-    });
-  });
-
-  describe("removeResource", () => {
-    test("dispatches remove resource action", () => {
-      const dispatch = jest.fn();
-
-      const checklist = {
-        id: 1,
-        type: "checklists"
-      };
-
-      const deleteAction = {
-        type: "REMOVE_RESOURCE_BY_ID",
-        resourceType: checklist.type,
-        id: checklist.id
-      };
-
-      removeResource(checklist, dispatch);
-      expect(dispatch).toBeCalledWith(deleteAction);
-      expect(dispatch).toMatchSnapshot();
     });
   });
 });
