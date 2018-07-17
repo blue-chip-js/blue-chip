98ba293ea7d3882f90cd2b72eb82761c1bdb2e34
Mike Piccolo <mfpiccolo@gmail.com>
2018-07-16 23:12:41 -0700

    Scope and bump version

--- src/index.js
+++ src/index.js
@@ -1,17 +1,4 @@
 import {Actions} from "./actions";
 import BaseModel from "./BaseModel";
 
-// TODO: remove to adapters
-import reduxAdapter from "./adapters/redux";
-import mobxAdapter from "./adapters/mobx";
-import setStateAdapter from "./adapters/setState";
-import vuexAdapter from "./adapters/vuex";
-
-export {
-  Actions,
-  BaseModel,
-  vuexAdapter,
-  reduxAdapter,
-  mobxAdapter,
-  setStateAdapter
-};
+export {Actions, BaseModel};
