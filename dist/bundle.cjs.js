a1fa6f9b6643f7af032b1734d3e38c330fc8f164
Mike Piccolo <mfpiccolo@gmail.com>
2018-08-20 22:21:01 -0700

    remove unused param

--- dist/bundle.cjs.js
+++ dist/bundle.cjs.js
@@ -73,12 +73,8 @@ class Actions {
       isGraphQl(payload) ? graphQlNormalize(payload) : jsonApiNormalize(payload)
     ).forEach(([resourceType, resourcesById]) => {
       const rById = isGraphQl(payload)
-        ? this._addIndex(
-            toJsonApiSpec(resourceType, resourcesById),
-            payload,
-            "GraphQL"
-          )
-        : this._addIndex(resourcesById, payload, "JsonApi");
+        ? this._addIndex(toJsonApiSpec(resourceType, resourcesById), payload)
+        : this._addIndex(resourcesById, payload);
 
       this.actions.updateResources(this.mutator, resourceType, rById);
     });
@@ -100,13 +96,13 @@ class Actions {
     this.actions.clearResources(this.mutator, resourceTypes);
   }
 
-  _addIndex(resourcesById, payload, type) {
+  _addIndex(resourcesById, payload) {
     return Object.entries(
       resourcesById
     ).reduce((indexedResourcesById, [id, resource]) => {
-      const __index = payload.data.findIndex(rsource => {
-        return rsource.id.toString() === id.toString();
-      });
+      const __index = payload.data.findIndex(
+        rsource => rsource.id.toString() === id.toString()
+      );
       if (__index === -1) console.log(__index, type, resource);
       indexedResourcesById[id] = {...resource, __index};
       return indexedResourcesById;
